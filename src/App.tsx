c8c874913360c5bf9b4bbe719ecd1af97a2864a0
PETE <noppawat_pete@outlook.com>
2024-01-31 21:59:51 +0700

    remove unuse code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Question from './Question';
 import Leaderboard from './Leaderboard';
 import { IScore } from './types';
